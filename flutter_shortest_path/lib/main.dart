d20794025658f08d213606d2626c2c77219ac215
codes <hello@cody.codes>
2020-05-21 16:22:12 -0700

    :lipstick: add UW color

--- flutter_shortest_path/lib/main.dart
+++ flutter_shortest_path/lib/main.dart
@@ -4,29 +4,34 @@ void main() {
   runApp(MyApp());
 }
 
+Map<int, Color> color =
+{
+  50:Color.fromRGBO(51,0,111, .1),
+  100:Color.fromRGBO(51,0,111, .2),
+  200:Color.fromRGBO(51,0,111, .3),
+  300:Color.fromRGBO(51,0,111, .4),
+  400:Color.fromRGBO(51,0,111, .5),
+  500:Color.fromRGBO(51,0,111, .6),
+  600:Color.fromRGBO(51,0,111, .7),
+  700:Color.fromRGBO(51,0,111, .8),
+  800:Color.fromRGBO(51,0,111, .9),
+  900:Color.fromRGBO(51,0,111, 1),
+};
+MaterialColor colorCustom = MaterialColor(0xFF4B2E83, color);
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Shortest Path',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+        primarySwatch: colorCustom,
+        bottomAppBarColor: colorCustom,
+
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Shortest Path'),
     );
   }
 }
