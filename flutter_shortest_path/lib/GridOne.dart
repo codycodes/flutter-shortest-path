6ae7e5b14420d98618396b865e138b0f13212da8
codes <hello@cody.codes>
2020-05-24 08:34:45 -0700

    update for iOS, some grid stuff

--- flutter_shortest_path/lib/GridOne.dart
+++ flutter_shortest_path/lib/GridOne.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+//import 'dart:collection';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -9,6 +10,8 @@ class GridOne extends StatefulWidget {
 }
 
 class _GridOneState extends State<GridOne> {
+  final int int64MaxValue = 9223372036854775807;
+
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -25,8 +28,33 @@ class _GridOneState extends State<GridOne> {
     }
     int numCellsHeight = (numCellsWidth * heights).toInt();
 
+    // should be moved somewhere
+    // used to push the row up one if it's in the middle instead of down
+    int minusOne = 0;
+    if (heights % 2 == 1) {
+      minusOne++;
+    }
+    int startRow = ((heights/2)- minusOne).floor();
+    int startCol = (numCellsWidth ~/ 4);
 
-    List<List<String>> gridState = [ List.filled(numCellsHeight, '0') ];
+    List<List<int>> createGridData() {
+      // Creates a list of list of strings representing state for each cell in the grid
+      List<List<int>> gridState = [];
+      List.filled(numCellsWidth, '0');
+      for (int i = 0; i < heights; i++) {
+       gridState.add(List<int>.filled(numCellsWidth, int64MaxValue));
+      }
+      return gridState;
+    }
+
+    List<List<int>> gridState = createGridData();
+    gridState[0][0] = 0;
+    gridState[1][1] = int64MaxValue;
+
+//    minPq<HeapPriorityQueue><>
+    Map<List, int> totalCosts = Map();
+    // Each index for the graph will not be changed
+    totalCosts[[0,0]] = 1;
 
     // NODE class stuff
     // Getting height is dividing by the width (undo?) and then modulus gets the col
@@ -39,12 +67,16 @@ class _GridOneState extends State<GridOne> {
 
     Color getNodeColor(number) {
       List rowCol = convertIndexRowCol(number);
-      int minusOne = 0;
-      if (heights % 2 == 1) {
-        minusOne++;
+
+      print(rowCol);
+      print(totalCosts);
+      if (totalCosts[rowCol] == 1) {
+        return Colors.red;
       }
-      if (rowCol[0] == ((heights/2)- minusOne).floor()
-      && rowCol[1] == (numCellsWidth/4)) {
+//      if (gridState[rowCol[0]][rowCol[1]] == 0){
+//        return Colors.black;
+//      }
+      if (rowCol[0] == startRow && rowCol[1] == startCol) {
         //start
         return Colors.blue;
       } else if (rowCol[0] == ((heights/2)- minusOne).floor()
